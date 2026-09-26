2172e2e9b1a5db45ec8ba2c635608680cb2a10ed
sadi <sadullasadi99@gmail.com>
2026-09-26 13:04:19 +0600

    Made the footer responsive

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,13 +5,13 @@ const Footer = () => {
     return (
         <div className="">
             <div className="divider"></div>
-            <div className="container mx-auto my-10 flex justify-between items-center">
+            <div className="container mx-auto my-10 px-4  flex flex-col sm:flex-row justify-between items-center gap-4 text-center sm:text-left">
                 <div className="flex items-center gap-2">
-                    <Image src={footerlogo} alt="footer"></Image>
+                    <Image src={footerlogo} alt="footer" className="w-8 h-8 sm:w-10 sm:h-10"></Image>
                     <h3>FITLOG</h3>
                 </div>
                 <div>
-                    <h4 className="text-[#6B7280]">© 2026 FitLog — Workout Library. Train hard, log honest.</h4>
+                    <h4 className="text-[#6B7280 text-sm sm:text-base">© 2026 FitLog — Workout Library. Train hard, log honest.</h4>
                 </div>
             </div>
         </div>
